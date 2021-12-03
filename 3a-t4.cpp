6e4fa1542d4dc1a36b199e79f69e46209915e014
aleggz1 <95345619+aleggz1@users.noreply.github.com>
2021-12-03 21:33:03 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,19 +1,29 @@
+#include "pch.h"
 #include<iostream> 
+#include <limits>
+
 using namespace std;
 
 int main()
 {
     int k, i = 1, sum_k = 0, number;
     cout << " Enter the amount of integers to sum = "; cin >> k;
-
-    while ((k >= 1) && (i <= k)) {
+    while ((k >= 1) && (i <= k) && (true)) {
         cout << "Enter integer nr. " << i << ": ";
         cin >> number;
-        sum_k += number;
         i++;
-        if (k == 0) {
-            break;
+
+
+        while (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Wrong input!Enter integer again: ";
+            cin >> number;
         }
+
+        sum_k += number;
+
     }
     cout << " The total sum of " << k << " integers is: " << sum_k;
 
